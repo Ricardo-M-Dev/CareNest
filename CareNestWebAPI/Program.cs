af1c782cfc61ba2471d30cd3c354f146e75cfcf1
Ricardo <ricardo.monteiro@playlist.com.br>
2025-04-08 18:01:31 -0300

    chore: Adiciona Injeção de Dependência do Mediator
    - Adiciona Injeção de Dependência do Mediator

--- CareNestWebAPI/Program.cs
+++ CareNestWebAPI/Program.cs
@@ -1,14 +1,17 @@
+using Application.Common.Mediator;
+using Application.Patients.Commands;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddSingleton<Mediator>();
+builder.Services.AddTransient<IRequestHandler<CreatePatientCommand, int>, CreatePatientCommandHandler>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
