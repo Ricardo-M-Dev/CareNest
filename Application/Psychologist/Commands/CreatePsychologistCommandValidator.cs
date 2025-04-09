8f3400df4e5f03a45af45ce90232276b4c67c7c5
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-09 20:19:26 -0300

    feat: Adiciona biblioteca Domain.ValueObjects e refatora Psicólogo.
    - Adicione biblioteca Domain.ValueObjects para utilização adicional nos validadores.
    - Adicione atributo "ativo" ao construtor da classe Psicólogo e propriedade "CRP" e "Especialização" com getter não público.

--- Application/Psychologist/Commands/CreatePsychologistCommandValidator.cs
+++ Application/Psychologist/Commands/CreatePsychologistCommandValidator.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using Domain.ValueObjects;
+using FluentValidation;
 
 namespace Application.Psychologist.Commands
 {
--- Domain/Entities/Psychologist.cs
+++ Domain/Entities/Psychologist.cs
@@ -2,7 +2,7 @@
 
 namespace Domain.Entities
 {
-    class Psychologist(FullName fullName, Email email, Password password, Identity identity, DateOfBirth dateOfBirth, Gender gender, Phone phone, Address address, City city, State state, ZipCode zipCode, Country country, CRP cRP, string specialization, List<Patient> patients, string bio, bool isAvailable) : Person(fullName, email, password, identity, dateOfBirth, gender, phone, address, city, state, zipCode, country)
+    class Psychologist(FullName fullName, Email email, Password password, Identity identity, DateOfBirth dateOfBirth, Gender gender, Phone phone, Address address, City city, State state, ZipCode zipCode, Country country, bool isActive, CRP cRP, string specialization, List<Patient> patients, string bio, bool isAvailable) : Person(fullName, email, password, identity, dateOfBirth, gender, phone, address, city, state, zipCode, country, isActive)
     {
         public CRP? CRP { get; private set; } = cRP;
         public string? Specialization { get; private set; } = specialization;
