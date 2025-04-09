32f4c69a53fb71804a38c170f305d38607ea6837
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-09 20:04:24 -0300

    feat: Adiciona criação de psicólogo com validações de campo.
    - Adiciona comando de criação de psicólogo com propriedades nome, email, senha, identidade, data de nascimento, gênero, telefone, endereço, cidade, estado, CEP e país.
    - Adiciona recurso de tratamento de comandos para criar psicólogo.
    - Adiciona validador para comandos de criação de psicólogo com regras de campo.

---
+++ Application/Psychologist/Commands/CreatePsychologistCommand.cs
@@ -0,0 +1,27 @@
+﻿using Application.Common.Mediator;
+using Domain.Entities;
+using Domain.ValueObjects;
+
+namespace Application.Psychologist.Commands
+{
+    public class CreatePsychologistCommand : IRequest<int>
+    {
+        public string FullName { get; set; } = default!;
+        public string Email { get; set; } = default!;
+        public string Password { get; set; } = default!;
+        public string Identity { get; set; } = default!;
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; } = default!;
+        public string Phone { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string City { get; set; } = default!;
+        public string State { get; set; } = default!;
+        public string ZipCode { get; set; } = default!;
+        public string Country { get; set; } = default!;
+        public bool IsActive { get; set; } = default!;
+        public string? CRP { get; set; }
+        public string? Specialization { get; set; }
+        public string? Bio { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Application/Psychologist/Commands/CreatePsychologistCommandHandler.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Mediator;
+
+namespace Application.Psychologist.Commands
+{
+    public class CreatePsychologistCommandHandler : IRequestHandler<CreatePsychologistCommand, int>
+    {
+        public int Handle(CreatePsychologistCommand command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
