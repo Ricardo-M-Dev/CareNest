2a079eb528ee8e297e35d5b5afbd9301b1e367fa
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-10 21:20:33 -0300

    feat: Adiciona exceções para tratamento de entidades não encontradas e erros de validação.
    - Criar classe de exceção NotFoundException para capturar casos de não encontrados de entidades.
    - Crie exceção de validação para lidar com erros de validação personalizados.

---
+++ Application/Common/Exceptions/NotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace Application.Common.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string entityName, object key)
+            : base($"'{entityName}' com o id '{key}' não foi encontrado.")
+        {
+        }
+    }
+}
