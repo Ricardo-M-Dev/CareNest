c3f551012414a1565fe2fb9c1fc8b9187d053d39
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-14 21:11:27 -0300

    feat: Implementação do repositório e conexão com DB SQLite.
    - Criado interface IRepository<T> com métodos básicos para CRUD.
    - Criar classe DbConnectionFactory para conexões com o banco de dados SQLite.

---
+++ Application/Common/Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Application.Common.Interfaces
+{
+    public interface IRepository<T>
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
+}
---
+++ Infrastructure/Persistence/DbConnectionFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data;
+
+namespace Infrastructure.Persistence
+{
+    public class DbConnectionFactory
+    {
+        private readonly string _connectionString;
+
+        public DbConnectionFactory(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IDbConnection CreateConnection() => new SqliteConnection(_connectionString);
+    }
+}
