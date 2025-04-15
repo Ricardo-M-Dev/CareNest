f78f34c5a286ef3861ebd68aefdf6add4aad5156
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-14 21:12:04 -0300

    fix: Corrigindo entidade Psicólogo e implementando repositório de psicólogos.
    - Adiciona interface IPsychologistRepository para repositório de psicólogo na aplicação.
    - Fixa Campos em entidade Psicólogo para seguir padrões de acesso enscondido e encapsulamento.
    - Adiciona implementação da classe de repositório de psicólogo que permite inserir, atualizar, deletar e recuperar dados de psicólogos.

--- Domain/Entities/Psychologist.cs
+++ Domain/Entities/Psychologist.cs
@@ -2,12 +2,41 @@
 
 namespace Domain.Entities
 {
-    class Psychologist(FullName fullName, Email email, Password password, Identity identity, DateOfBirth dateOfBirth, Gender gender, Phone phone, Address address, City city, State state, ZipCode zipCode, Country country, bool isActive, CRP cRP, string specialization, List<Patient> patients, string bio, bool isAvailable) : Person(fullName, email, password, identity, dateOfBirth, gender, phone, address, city, state, zipCode, country, isActive)
+    public class Psychologist : Person
     {
-        public CRP? CRP { get; private set; } = cRP;
-        public string? Specialization { get; private set; } = specialization;
-        public List<Patient>? Patients { get; private set; } = patients;
-        public string? Bio { get; private set; } = bio;
-        public bool IsAvailable { get; private set; } = isAvailable;
+        public CRP CRP { get; private set; }
+        public string Specialization { get; private set; }
+        public List<Patient> Patients { get; private set; }
+        public string Bio { get; private set; }
+        public bool IsAvailable { get; private set; }
+
+        public Psychologist(
+            FullName fullName,
+            Email email,
+            Password password,
+            Identity identity,
+            DateOfBirth dateOfBirth,
+            Gender gender,
+            Phone phone,
+            Address address,
+            City city,
+            State state,
+            ZipCode zipCode,
+            Country country,
+            bool isActive,
+            CRP crp,
+            string specialization,
+            List<Patient> patients,
+            string bio,
+            bool isAvailable)
+            : base(fullName, email, password, identity, dateOfBirth, gender, phone, address, city, state, zipCode, country, isActive)
+        {
+            CRP = crp;
+            Specialization = specialization;
+            Patients = patients;
+            Bio = bio;
+            IsAvailable = isAvailable;
+        }
     }
+
 }
---
+++ Infrastructure/Persistence/Repositories/PsychologistRepository.cs
@@ -0,0 +1,84 @@
+﻿using Application.Interfaces;
+using Dapper;
+using Domain.Entities;
+
+namespace Infrastructure.Persistence.Repositories
+{
+    public class PsychologistRepository : IPsychologistRepository
+    {
+        private readonly DbConnectionFactory _connectionFactory;
+
+        public PsychologistRepository(DbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+        public Task AddAsync(Psychologist entity)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = @"
+                INSERT INTO Psychologists 
+                    (FirstName, LastName, Email, DateOfBirth) 
+                VALUES 
+                    (@FirstName, @LastName, @Email, @DateOfBirth);";
+
+            return connection.ExecuteAsync(query, entity);
+        }
+
+        public Task AddRangeAsync(IEnumerable<Psychologist> entities)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = @"
+                INSERT INTO Psychologists 
+                    (FirstName, LastName, Email, DateOfBirth) 
+                VALUES 
+                    (@FirstName, @LastName, @Email, @DateOfBirth);";
+
+            return connection.ExecuteAsync(query, entities);
+        }
+
+        public Task<IEnumerable<Psychologist>> GetAllAsync()
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = "SELECT * FROM Psychologists";
+
+            return connection.QueryAsync<Psychologist>(query);
+        }
+
+        public Task<Psychologist?> GetByIdAsync(int id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = "SELECT * FROM Psychologists WHERE Id = @Id";
+
+            return connection.QueryFirstOrDefaultAsync<Psychologist>(query, new { Id = id });
+        }
+
+        public Task UpdateAsync(Psychologist entity)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = @"
+                UPDATE Psychologists 
+                SET 
+                    FirstName = @FirstName, 
+                    LastName = @LastName, 
+                    Email = @Email, 
+                    DateOfBirth = @DateOfBirth 
+                WHERE Id = @Id;";
+
+            return connection.ExecuteAsync(query, entity);
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = "DELETE FROM Psychologists WHERE Id = @Id;";
+
+            return connection.ExecuteAsync(query, new { Id = id });
+        }
+    }
+}
