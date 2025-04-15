43e12a9a3f0b08506c458e14e625cc5e44eac23c
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-15 20:54:06 -0300

    feat: Exception de Transacao personalizado
    - Adiciona Exception para erro ao realizar uma Transaction no banco, para melhor depuração.

---
+++ Application/Common/Exceptions/TransactionFailureException.cs
@@ -0,0 +1,20 @@
+﻿namespace Application.Common.Exceptions
+{
+    public class TransactionFailureException : Exception
+    {
+        public string Operation { get; }
+        public string ContextInfo { get; }
+
+        public TransactionFailureException(string operation, string contextInfo, Exception innerException)
+            : base($"Falha ao executar operação: {operation}. Veja o inner exception para mais detalhes.", innerException)
+        {
+            Operation = operation;
+            ContextInfo = contextInfo;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}\nOperation: {Operation}\nContext: {ContextInfo}";
+        }
+    }
+}
