a2d0ea6c4fe346dce58a73c05c4bd2b9232e5dc4
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-14 21:11:47 -0300

    feat: Implementa interface IPatientRepository e repositório para paciente.
    - Implementa interface IPatientRepository para trabalhar com repositório de pacientes.
    - Atualize a estrutura de inicialização do modelo de paciente.
    - Adiciona repositório para paciente com métodos para adicionar, adicionar range, busca todos, busca por ID, atualizar e excluir.

--- Domain/Entities/Patient.cs
+++ Domain/Entities/Patient.cs
@@ -2,10 +2,35 @@
 
 namespace Domain.Entities
 {
-    public class Patient(FullName fullName, Email email, Password password, Identity identity, DateOfBirth dateOfBirth, Gender gender, Phone phone, Address address, City city, State state, ZipCode zipCode, Country country, EmergencyContact? emergencyContact, Insurance? insurance, bool isUnderTreatment) : Person
+    public class Patient : Person
     {
-        public EmergencyContact? EmergencyContact { get; private set; } = emergencyContact;
-        public Insurance? Insurance { get; private set; } = insurance;
-        public bool IsUnderTreatment { get; private set; } = isUnderTreatment;
+        public EmergencyContact? EmergencyContact { get; private set; }
+        public Insurance? Insurance { get; private set; }
+        public bool IsUnderTreatment { get; private set; }
+
+        public Patient(
+            FullName fullName,
+            Email email,
+            Password password,
+            Identity identity,
+            DateOfBirth dateOfBirth,
+            Gender gender,
+            Phone phone,
+            Address address,
+            City city,
+            State state,
+            ZipCode zipCode,
+            Country country,
+            bool isActive,
+            EmergencyContact? emergencyContact,
+            Insurance? insurance,
+            bool isUnderTreatment)
+            : base(fullName, email, password, identity, dateOfBirth, gender, phone, address, city, state, zipCode, country, isActive)
+        {
+            EmergencyContact = emergencyContact;
+            Insurance = insurance;
+            IsUnderTreatment = isUnderTreatment;
+        }
     }
+
 }
---
+++ Infrastructure/Persistence/Repositories/PatientRepository.cs
@@ -0,0 +1,85 @@
+﻿using Dapper;
+using Infrastructure.Persistence;
+using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Repositories
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly DbConnectionFactory _connectionFactory;
+
+        public PatientRepository(DbConnectionFactory connectionFactory)
+        {
+            _connectionFactory = connectionFactory;
+        }
+
+        public async Task AddAsync(Patient patient)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = @"
+                INSERT INTO Patients 
+                    (FirstName, LastName, Email, DateOfBirth) 
+                VALUES 
+                    (@FirstName, @LastName, @Email, @DateOfBirth);";
+
+            await connection.ExecuteAsync(query, patient);
+        }
+
+        public Task AddRangeAsync(IEnumerable<Patient> entities)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = @"
+                INSERT INTO Patients 
+                    (FirstName, LastName, Email, DateOfBirth) 
+                VALUES 
+                    (@FirstName, @LastName, @Email, @DateOfBirth);";
+
+            return connection.ExecuteAsync(query, entities);
+        }
+
+        public Task<IEnumerable<Patient>> GetAllAsync()
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = "SELECT * FROM Patients";
+            return connection.QueryAsync<Patient>(query);
+        }
+
+        public Task<Patient?> GetByIdAsync(int id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = "SELECT * FROM Patients WHERE Id = @Id";
+
+            return connection.QueryFirstOrDefaultAsync<Patient>(query, new { Id = id });
+        }
+
+        public Task UpdateAsync(Patient entity)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = @"
+                UPDATE Patients 
+                SET 
+                    FirstName = @FirstName, 
+                    LastName = @LastName, 
+                    Email = @Email, 
+                    DateOfBirth = @DateOfBirth 
+                WHERE Id = @Id;";
+
+            return connection.ExecuteAsync(query, entity);
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            using var connection = _connectionFactory.CreateConnection();
+
+            var query = "DELETE FROM Patients WHERE Id = @Id";
+
+            return connection.ExecuteAsync(query, new { Id = id });
+        }
+    }
+}
