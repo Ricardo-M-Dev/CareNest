7c14d37af5c513aed989128205126fb21431a99f
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-14 21:14:06 -0300

    feat: Adiciona suporte a dados confidenciais.
    - Registra conexão com banco de dados e DI de repositorios para pacientes e psicólogos.

--- CareNestWebAPI/Program.cs
+++ CareNestWebAPI/Program.cs
@@ -1,14 +1,25 @@
+using Application.Commands.Patients;
+using Application.Commands.Psychologists;
 using Application.Common.Mediator;
-using Application.Patients.Commands;
+using Application.Interfaces;
+using Application.Repositories;
+using Infrastructure.Persistence;
+using Infrastructure.Persistence.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=app.db";
+
+builder.Services.AddSingleton(new DbConnectionFactory(connectionString));
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IPsychologistRepository, PsychologistRepository>();
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
 
 builder.Services.AddSingleton<Mediator>();
 builder.Services.AddTransient<IRequestHandler<CreatePatientCommand, int>, CreatePatientCommandHandler>();
+builder.Services.AddTransient<IRequestHandler<CreatePsychologistCommand, int>, CreatePsychologistCommandHandler>();
 
 var app = builder.Build();
 
