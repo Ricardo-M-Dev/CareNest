cb053bbc14d139494bf7af01c2432d2354669b56
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-16 22:08:47 -0300

    feat: cria controller de pacientes
    - Cria controller de pacientes para ser implementada futuramente.

---
+++ CareNestWebAPI/Controllers/PatientController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CareNestWebAPI.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
