ec446638266046cf26069214a66b7cc06f5cd707
Ricardo-M-Dev <ricardomontteiro.dev@gmail.com>
2025-04-16 21:59:08 -0300

    feat: adiciona conexão MySQL
    - Implementa conexão com banco de dados MySQL e configura serviços de repositório e cache para melhorar a performance da aplicação.

--- CareNestWebAPI/Program.cs
+++ CareNestWebAPI/Program.cs
@@ -1,33 +1,47 @@
 using Application.Commands.Patients;
 using Application.Commands.Psychologists;
+using Application.Common.Interfaces;
 using Application.Common.Mediator;
-using Application.Interfaces;
+using Application.Interfaces.Repositories;
+using Application.Interfaces.Services;
 using Application.Repositories;
+using Application.Services;
 using Infrastructure.Cache;
 using Infrastructure.Persistence;
 using Infrastructure.Persistence.Repositories;
+using MySqlConnector;
 using StackExchange.Redis;
+using System.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? "Data Source=app.db";
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var redisConfiguration = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
 
-builder.Services.AddSingleton(new DbConnectionFactory(connectionString));
+builder.Services.AddScoped<IDbConnection>(_ =>
+{
+    var connection = new MySqlConnection(connectionString);
+    connection.Open();
+    return connection;
+});
+
+builder.Services.AddScoped<IUnityOfWork, UnityOfWork>();
+
 builder.Services.AddScoped<IPatientRepository, PatientRepository>();
 builder.Services.AddScoped<IPsychologistRepository, PsychologistRepository>();
 
-builder.Services.AddSingleton<IConnectionMultiplexer>(
-    _ => ConnectionMultiplexer.Connect("localhost:6379")
-);
-
+builder.Services.AddScoped<IPatientService, PatientService>();
+builder.Services.AddScoped<IPsychologistService, PsychologistService>();
 builder.Services.AddScoped<IRedisCacheService, RedisCacheService>();
+
+builder.Services.AddScoped<Mediator>();
+builder.Services.AddTransient<IRequestHandler<CreatePatientCommand, int>, CreatePatientCommandHandler>();
+builder.Services.AddTransient<IRequestHandler<CreatePsychologistCommand, int>, CreatePsychologistCommandHandler>();
+
+builder.Services.AddSingleton<IConnectionMultiplexer>(_ => ConnectionMultiplexer.Connect(redisConfiguration));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
-
-builder.Services.AddSingleton<Mediator>();
-builder.Services.AddTransient<IRequestHandler<CreatePatientCommand, int>, CreatePatientCommandHandler>();
-builder.Services.AddTransient<IRequestHandler<CreatePsychologistCommand, int>, CreatePsychologistCommandHandler>();
 
 var app = builder.Build();
 
@@ -37,9 +51,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
